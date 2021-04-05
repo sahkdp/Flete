beedebfa0f0ef65fca7fc25e34aa2d015b125f78
sahkdp <81811510+sahkdp@users.noreply.github.com>
2021-04-05 14:55:21 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+class Square
+{
+  public satic void main(String[]args)
+  {
+    Scanner scn=new scanner(System.in);
+      System.out.println("enter num");
+      int n=scn.nextInt();
+    for(int i=0;i<n;i++);
+     {
+       for(int j=0;j<n;j++);
+        {
+          if(i+J>=n-1)
+           System.out.print("*");
+ 	   else
+		System.out.print(" ");
+	}
+}
+}
+}
